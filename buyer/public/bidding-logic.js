1b785b0f3c26411d7e1fc23da742b5d572a25ddb
Tim Hsieh <timpeihunghsieh@gmail.com>
2021-11-21 19:21:40 +0000

    Added fencedframe

--- buyer/public/bidding-logic.js
+++ buyer/public/bidding-logic.js
@@ -2,10 +2,15 @@ function generateBid(
     interestGroup, auctionSignals, perBuyerSignals, trustedBiddingSignals,
     browserSignals) {
   console.log("Calling buyer's generateBid().");
+
   const ad = interestGroup.ads[0];
+  const adComponent1 = interestGroup.adComponents[0];
+  const adComponent2 = interestGroup.adComponents[1];
+
   return {'ad': 'example',
           'bid': ad.metadata.bid,
-          'render': ad.renderUrl};
+          'render': ad.renderUrl,
+          'adComponents': [adComponent1.renderUrl, adComponent2.renderUrl]};
 }
 
 function reportWin(
